755439c086ed2e3823c86534db4fb3976042990c
Ruwan Sampath <n4ruwan4s@gmail.com>
2026-09-07 09:23:55 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLtGfZnn34ecEz9bBgc5tawwUTPMLukHo",
+  authDomain: "kussiya-lunch.firebaseapp.com",
+  projectId: "kussiya-lunch",
+  storageBucket: "kussiya-lunch.firebasestorage.app",
+  messagingSenderId: "1032681220499",
+  appId: "1:1032681220499:web:90d7eb2f3ef21df576e066"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// 2) Paste your EmailJS values here (from emailjs.com), see setup steps.
+const EMAILJS_PUBLIC_KEY = "hcIuwffnSygMgQPca";
+const EMAILJS_SERVICE_ID = "service_nzft1yd";
+const EMAILJS_TEMPLATE_ID = "template_pc4qp9d";
+
+// 3) Where order notification emails should land.
+const ADMIN_NOTIFY_EMAIL = "n4ruwan4s@gmail.com";
