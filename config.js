995c5ec02407011a5228c670336be89ba8b2bd7c
Ruwan Sampath <n4ruwan4s@gmail.com>
2026-09-10 01:37:11 +0530

    config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ const firebaseConfig = {
   appId: "1:1032681220499:web:90d7eb2f3ef21df576e066"
 };
 
+
 // 2) Paste your EmailJS values here (from emailjs.com), see setup steps.
 const EMAILJS_PUBLIC_KEY = "hcIuwffnSygMgQPca";
 const EMAILJS_SERVICE_ID = "service_nzft1yd";
@@ -15,4 +16,5 @@ const EMAILJS_TEMPLATE_ID = "template_pc4qp9d";
 // 3) Where order notification emails should land.
 const ADMIN_NOTIFY_EMAIL = "n4ruwan4s@gmail.com";
 
+// 4) Google Analytics (Firebase Analytics)
 const FIREBASE_MEASUREMENT_ID = "G-VBL90KPW2G";
